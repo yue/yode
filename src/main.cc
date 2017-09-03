47c51781fa476be94dfac450be2df1626c5c83e7
Cheng Zhao <zcbenz@gmail.com>
2017-09-03 12:07:00 +0900

    win: Use WinMain as main entry

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,53 @@
 // Use of this source code is governed by the license that can be found in the
 // LICENSE file.
 
+#if defined(WIN32)
+#include <windows.h>
+#endif
+
 #include "node/src/node.h"
 
+#if defined(WIN32)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+  // Convert argv to UTF8.
+  int argc = 0;
+  wchar_t** wargv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+  char** argv = new char*[argc];
+  for (int i = 0; i < argc; i++) {
+    // Compute the size of the required buffer
+    DWORD size = WideCharToMultiByte(CP_UTF8,
+                                     0,
+                                     wargv[i],
+                                     -1,
+                                     NULL,
+                                     0,
+                                     NULL,
+                                     NULL);
+    if (size == 0) {
+      // This should never happen.
+      fprintf(stderr, "Could not convert arguments to utf8.");
+      return 1;
+    }
+    // Do the actual conversion
+    argv[i] = new char[size];
+    DWORD result = WideCharToMultiByte(CP_UTF8,
+                                       0,
+                                       wargv[i],
+                                       -1,
+                                       argv[i],
+                                       size,
+                                       NULL,
+                                       NULL);
+    if (result == 0) {
+      // This should never happen.
+      fprintf(stderr, "Could not convert arguments to utf8.");
+      return 1;
+    }
+  }
+
+#else  // !defined(WIN32)
 int main(int argc, char* argv[]) {
+#endif
+
   return node::Start(argc, argv);
 }
